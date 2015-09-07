f3c592de388470397569c1ede88c89300f960005
hanliu <343757717@qq.com>
2015-09-07 18:38:54 +0800

    调整界面】

--- QRCodeScannerDemo/QRCodeScanner/ViewController.m
+++ QRCodeScannerDemo/QRCodeScanner/ViewController.m
@@ -9,8 +9,16 @@
 #import "ViewController.h"
 #import "HLQRCodeViewController.h"
 #import "WebViewController.h"
+
+#define kScannerMaxY 110
+#define kGeneratorMinY 320
+#define SCREEN_BOUNDS self.view.bounds
+#define kScreenWidth SCREEN_BOUNDS.size.width
+#define kScreenHeight SCREEN_BOUNDS.size.height
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *QRCodeScan;
+@property (weak, nonatomic) IBOutlet UIButton *QRCodeGenerate;
 
 @end
 
@@ -19,13 +27,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
- 
     self.QRCodeScan.layer.cornerRadius  = self.QRCodeScan.frame.size.width/2;
     self.QRCodeScan.layer.masksToBounds = YES;
     
     
+    self.QRCodeGenerate.layer.cornerRadius  = self.QRCodeGenerate.frame.size.width/2;
+    self.QRCodeGenerate.layer.masksToBounds = YES;
+    
+    
+    self.QRCodeScan.frame = CGRectMake(kScreenWidth/2-50, -110, 100, 100);
+    NSLog(@"%@",NSStringFromCGRect(self.view.bounds));
+    self.QRCodeGenerate.frame = CGRectMake(kScreenWidth/2-50, 640, 100, 100);
+    
+    CGRect frame1 = self.QRCodeScan.frame;
+    frame1.origin.y = kScannerMaxY;
+    
+    NSLog(@"%@",NSStringFromCGRect(frame1));
+    
+    [UIView animateWithDuration:3 delay:0.0 usingSpringWithDamping:0.5 initialSpringVelocity:2 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        self.QRCodeScan.frame = frame1;
+    } completion:^(BOOL finished) {
+//        CGRect frame2 = self.QRCodeGenerate.frame;
+//        frame2.origin.y = kGeneratorMinY;
+//        [UIView animateWithDuration:2 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:2 options:UIViewAnimationOptionCurveEaseIn animations:^{
+//            self.QRCodeGenerate.frame = frame2;
+//            
+//        } completion:^(BOOL finished) {
+//            
+//        }];
+    }];
 }
 
+//读取二维码
 - (IBAction)captureQRCode:(id)sender {
     HLQRCodeViewController * qrcodeVC = [[HLQRCodeViewController alloc]init];
     qrcodeVC.qrReadSuccess = ^(HLQRCodeViewController *qrVC,NSString *result){
@@ -41,7 +74,10 @@
     };
     [self presentViewController:qrcodeVC animated:YES completion:nil];
 }
-
+//生成二维码
+- (IBAction)generateQRCode:(id)sender {
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
